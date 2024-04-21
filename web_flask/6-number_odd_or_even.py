6546503759be150a750c758ae536899c275d3374
Mariam22MR <xxxsaakk@gmail.com>
2024-04-21 15:23:46 -0700

    a

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,52 +1,54 @@
 #!/usr/bin/python3
-"""Flask"""
+""" Write script that starts Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Return a given string"""
+    """Display Hello world"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns a given string"""
+    """Display HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cText(text):
-    """display C followed by the value of the text variable"""
+    """Display C followed by value of text variable"""
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythonText(text="is cool"):
-    """display Python followed by the value of the text variable"""
+    """Display Python followed by value of text variable"""
     return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isNumber(n):
-    """display “n is a number” only if n is an integer"""
+    """Display “n is number” only if n is integer"""
     if isinstance(n, int):
         return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n=None):
-    """display a HTML page only if n is an integer"""
+    """Display HTML page only if n is integer"""
     if isinstance(n, int):
         return render_template("5-number.html", n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n=None):
-    """display a HTML page only if n is an integer:
-    H1 tag: “Number: n is even|odd” inside the tag BODY
+    """Display HTML page only if n i integer:
+    H1 tag: “Number: n is even|odd” inside tag BODY
     """
     if isinstance(n, int):
         if n % 2:
