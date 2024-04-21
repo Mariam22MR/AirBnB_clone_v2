df989800c98d500e305ed88848d0738b779bd01c
Mariam22MR <xxxsaakk@gmail.com>
2024-04-21 16:38:59 -0700

    a

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,16 +4,15 @@ Your web application must be listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask
+app = Flask(__name__)
 
-app = Flask("__name__")
 
-
-@app.route('/', strict_slashes=False)
-def hello():
+@app.route('/')
+def hello_flask():
     """Return string when route queried
     """
-    return ("Hello HBNB!")
+    return 'Hello HBNB!'
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
